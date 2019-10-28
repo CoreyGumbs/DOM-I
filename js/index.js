f5030f946ef51afac73945275f5ae8d7a0a77acf
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-28 17:28:34 -0400

    [Add] Added Main Content heading and text

--- js/index.js
+++ js/index.js
@@ -42,8 +42,16 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 
-//Navigation
+//Arrays
 const navArray = [siteContent['nav']['nav-item-1'], siteContent['nav']['nav-item-2'], siteContent['nav']['nav-item-3'], siteContent['nav']['nav-item-4'], siteContent['nav']['nav-item-5'], siteContent['nav']['nav-item-6']];
+
+const h4Arrays = [siteContent['main-content']['features-h4'], siteContent['main-content']['about-h4'], siteContent['main-content']['services-h4'], siteContent['main-content']['product-h4'], siteContent['main-content']['vision-h4'], siteContent['contact']['contact-h4'] ];
+console.log(h4Arrays);
+
+const mainContentArray =  [siteContent['main-content']['features-content'], siteContent['main-content']['about-content'], siteContent['main-content']['services-content'],siteContent['main-content']['product-content'], siteContent['main-content']['vision-content']];
+
+//Navigation
+
 
 const navigationLinks = document.querySelectorAll('a');
 
@@ -60,12 +68,28 @@ ctaHeading.textContent = siteContent['cta']['h1'];
 const ctaBtn = document.querySelector('button');
 ctaBtn.style.color= "black";
 ctaBtn.innerHTML = siteContent['cta']['button'];
-console.log(ctaBtn);
 
-console.log(ctaHeading);
 const ctaImg =  document.querySelector('#cta-img');
 ctaImg.setAttribute('src', siteContent["cta"]["img-src"]);
 
 //Main-Content
 const middleImg =  document.querySelector('#middle-img');
 middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+const h4Headings = document.querySelectorAll('h4');
+
+h4Headings.forEach((heading, idx) => {
+  heading.style.textAlign = 'left';
+  heading.textContent = h4Arrays[idx];
+})
+console.log(h4Headings);
+
+const test =  document.querySelectorAll('.text-content');
+test[0].childNodes[3].textContent
+console.log(test[0].childNodes[3]);
+
+const mainContentTxt =  document.querySelectorAll('.text-content p');
+
+mainContentTxt.forEach((element, idx) => {
+  element.textContent = mainContentArray[idx];
+})
